34a11f94564a1614ce28da80acf062ea3fad653c
Henry Zhu <hi@henryzoo.com>
2016-01-14 16:29:05 -0500

    Revert "Requeue generators for re-traversal so we pick up regeneratorRuntime - fixes T6676"

--- packages/babel-plugin-transform-runtime/test/fixtures/runtime/full/expected.js
+++ packages/babel-plugin-transform-runtime/test/fixtures/runtime/full/expected.js
@@ -7,7 +7,7 @@ import foo, * as bar from "someModule";
 
 export const myWord = _Symbol("abc");
 export function giveWord() {
-  return _regeneratorRuntime.wrap(function giveWord$(_context) {
+  return regeneratorRuntime.wrap(function giveWord$(_context) {
     while (1) switch (_context.prev = _context.next) {
       case 0:
         _context.next = 2;
